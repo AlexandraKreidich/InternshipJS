87720c28973a7859c3b56c2b99949218d52f1614
mmf_kupl <ikuplevich@mail.ru>
2017-08-03 17:11:56 +0300

    clear code

--- data.js
+++ data.js
@@ -1,10 +1,11 @@
 var Data = {
 
-    //подгрузка данных
+    // подгрузка данных
     getDataFor: function (start, duration, f) {
         if (Data.Cache.containsInterval(start, duration)) {
             f(Data.Cache.getInterval(start, duration), start);
-        } else {
+        }
+        else {
             Data.Request.getData('data.csv').then(function (response) {
                 Data.processData(response, start, duration, f);
                 //console.log(Data.Cache.Data);
@@ -12,9 +13,8 @@ var Data = {
         }
     },
 
+    // обработка полученных данных для дальнейшего использования
     processData: function (data, start, duration, f) {
-        //console.log(data);
-
         var arrayX = [],
             arrayY = [],
             end = start + duration,
@@ -42,11 +42,14 @@ var Data = {
 
     },
 
+    // пространство имен для работы с запросами к серверу
     Request: {
+        // получение данных с сервера
         get: function (url) {
             return new Promise(function (resolve, reject) {
 
                 var req = new XMLHttpRequest();
+
                 req.open('GET', url);
 
                 req.onload = function () {
@@ -66,18 +69,18 @@ var Data = {
             });
         },
 
-        //после отправки url получает просто текст из файла и отдаёт на обработку в следующую функцию
+        // после отправки url получает просто текст из файла и отдаёт на обработку в следующую функцию
         getData: function (url) {
             //console.log('request');
             return Data.Request.get(url).then(Data.Request.parseData);
         },
 
-        //разбирает на два массива
+        // разбирает на два массива
         parseData: function (data) {
             //console.log(data);
-            var arr = data.split('\n');
-            var l = arr.length - 1;
-            var arrayX = [],
+            var arr = data.split('\n'),
+                l = arr.length - 1,
+                arrayX = [],
                 arrayY = [],
                 tmp = null;
             for (var i = 1; i < l; i++) {
@@ -87,7 +90,6 @@ var Data = {
             }
             //console.log(arrayY);
             //console.log(arrayX);
-
             return {
                 x: arrayX,
                 y: arrayY
@@ -95,7 +97,7 @@ var Data = {
         }
     },
 
-    //перевод метки времени в формат ГГГГ-ММ-ДД
+    // перевод метки времени в формат ГГГГ-ММ-ДД
     tsToData: function (ts) {
         var fullData = new Date(ts),
             year = fullData.getFullYear(),
@@ -108,7 +110,7 @@ var Data = {
         return year + '-' + month + '-' + day;
     },
 
-    //перевод метки времени в формат ЧЧ:ММ:СС
+    // перевод метки времени в формат ЧЧ:ММ:СС
     tsToTime: function (ts) {
         var fullData = new Date(ts),
             hours = fullData.getHours(),
@@ -122,18 +124,19 @@ var Data = {
         return hours + ':' + minutes + ':' + seconds;
     },
 
+    //пространство имен для работы с кешем
     Cache: {
-
         // проверяет наличие требуемого интервала в кэше (возвращает true/false)
         containsInterval: function (start, duration) {
             //console.log(this.findPoint(start));
-            if (this.findPoint(start).first !== -1 && this.findPoint(start + duration).second !== -1 && this.Data.x.length !== 0) {
-                return true;
-            }
-            return false;
+            // if (this.findPoint(start).first !== -1 && this.findPoint(start + duration).second !== -1 && this.Data.x.length !== 0) {
+            //     return true;
+            // }
+            // return false;
+            return this.findPoint(start).first !== -1 && this.findPoint(start + duration).second !== -1 && this.Data.x.length !== 0;
         },
 
-        //получаем заданный интервал из кэша
+        // получаем заданный интервал из кэша
         getInterval: function (start, duration) {
             var l = this.Data.x.length,
                 arrayX = [],
@@ -150,23 +153,27 @@ var Data = {
             var x = new Int32Array(arrayX),
                 y = new Int32Array(arrayY);
 
-            return {x: x, y: y};
+            return {
+                x: x,
+                y: y
+            };
         },
 
+        // сохранение данных в кеш
         save: function (X, Y) {
             // X - массив иксовых координат точек, которые нужно сохранить
             // Y - соответствующие иксам значения по оси игрек
             // Мы хотим вставить X и Y в кеш, не разрушая упорядоченность точек.
 
+            //DEBUG
             // var seed = 1498000000000;
             // console.log((X[0] - seed) + " ..(" + X.length + ").. " + (X[X.length - 1] - seed));
             // console.log((this.Data.x[0] - seed) + " ..(" + this.Data.x.length + ").. " + (this.Data.x[this.Data.x.length - 1] - seed));
-            //DEBUG
 
             if (window.conds) window.conds = [];
             if (this.Data.x.length === 0) {
-                //если данных нет в кэше
-                //console.log(1);
+                // если данных нет в кэше
+                // console.log(1);
                 this.Data.x = X;
                 this.Data.y = Y;
             }
@@ -181,29 +188,29 @@ var Data = {
 
                 if (x_start === -1 && x_end === -1) {
                     // если записываемые данные содержат в себе отрезок, который уже существует
-                    //console.log(2);
+                    // console.log(2);
                     tmp.x = X;
                     tmp.y = Y;
                 }
                 else if (x_start === -1) {
-                    //если записываемый отрезок строго правее данных в кэше
-                    //console.log(3);
+                    // если записываемый отрезок строго правее данных в кэше
+                    // console.log(3);
                     tmp.x = X;
                     tmp.y = Y;
                     tmp.x = tmp.x.concat(this.Data.x.slice(x_end, this.Data.x.length));
                     tmp.y = tmp.y.concat(this.Data.y.slice(x_end, this.Data.y.length));
                 }
                 else if (x_end === -1) {
-                    //если записываемый отрезок строго левее данных в кэше
-                    //console.log(4);
+                    // если записываемый отрезок строго левее данных в кэше
+                    // console.log(4);
                     tmp.x = this.Data.x.slice(0, x_start + 1);
                     tmp.y = this.Data.y.slice(0, x_start + 1);
                     tmp.x = tmp.x.concat(X);
                     tmp.y = tmp.y.concat(Y);
                 }
                 else {
-                    //записываемые данные пересекаются, но не содержат данные в кэше
-                    //console.log(5);
+                    // записываемые данные пересекаются, но не содержат данные в кэше
+                    // console.log(5);
                     tmp.x = this.Data.x.slice(0, x_start + 1);
                     tmp.y = this.Data.y.slice(0, x_start + 1);
                     tmp.x = tmp.x.concat(X);
@@ -215,24 +222,26 @@ var Data = {
                 this.Data.y = tmp.y;
             }
         },
-        //возвращает индекс элемента в кэшированном массиве, который является минимальным из всех, что строго больше переданного первым аргументом
-        // (максимальным среди всех, что строго меньше)
-        //бинарный поиск
-        findPoint: function (point) {
-            //point - точка для которой мы ищем индекса, flag - флаг для отслеживания индекса(левое или правое)
 
-            //левый конец (индекс) рассматриваемого на текущей итерации отрезка
+        // функция поиска индекса элемента:
+        // возвращает индекс элемента в кэшированном массиве, который является минимальным из всех,
+        // что строго больше переданного первым аргументом
+        // (максимальным среди всех, что строго меньше)
+        // (бинарный поиск)
+        findPoint: function (point) {
+            // point - точка для которой мы ищем индекса
+
+            // левый конец (индекс) рассматриваемого на текущей итерации отрезка
             var start = 0,
                 //положение конца (правый конец)
                 end = Data.Cache.Data.x.length - 1,
                 //первое среднее значение (индекс)
                 midF = Math.floor((start + end) / 2),
                 //второе среднее значение (индекс)
-                midS = Math.ceil((start + end) / 2);
-
-            //итератор для незацикливания
-            var i = 0;
-            var X = Data.Cache.Data.x;
+                midS = Math.ceil((start + end) / 2),
+                //итератор для незацикливания
+                i = 0,
+                X = Data.Cache.Data.x;
 
             if (point <= X[0]) {
                 if (point < X[0]) {
@@ -283,6 +292,7 @@ var Data = {
             }
         },
 
+        // объект данных кеша
         Data: {
             x: [],
             y: []
